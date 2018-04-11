01fb8453cfb2750205dcf914b68267f385f1aebf
TheJP <jp_55555@hotmail.com>
2018-04-11 15:56:14 +0200

    Worked on layout and design

--- Schoggifabrik/wwwroot/js/site.js
+++ Schoggifabrik/wwwroot/js/site.js
@@ -1,5 +1,9 @@
-﻿const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-    lineNumbers: true,
-    matchBrackets: true,
-    theme: "material"
-});
+﻿(() => {
+    const textarea = document.getElementById("code");
+    textarea.value = textarea.value.trim();
+    const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+        lineNumbers: true,
+        matchBrackets: true,
+        theme: "material"
+    });
+})();
