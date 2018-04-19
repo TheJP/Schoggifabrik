4c295ef8ccb485626686d0c97bf503d3d4394d19
TheJP <jp_55555@hotmail.com>
2018-04-19 19:40:21 +0200

    Added new problem 'Statistiken'

--- Schoggifabrik/Data/Problems.cs
+++ Schoggifabrik/Data/Problems.cs
@@ -16,7 +16,7 @@ namespace Schoggifabrik.Data
         public static IList<Problem> AsList { get; } = new Problem[]
         {
             new Problem(
-                name: "Start",
+                name: "Willkommen",
                 flavor: "Wir wissen, dass du noch keine Anstellung an deinem neuen Wohnort gefunden hast. " +
                     "Aber keine Angst: Die <span class=\"highlight\">Schoggifabrik</span> vorort stellt dich, den beinahe schon Schweizer, sehr gerne ein. "+
                     "Da du in deinem Portfolio schreibst es falle dir leicht neue Programmiersprachen zu lernen, " +
@@ -33,7 +33,7 @@ namespace Schoggifabrik.Data
                 }),
 
             new Problem(
-                name: "Start 2",
+                name: "Willkommen Persönlich",
                 flavor: "Gut gemacht. Dein Vorgesetzter Gummi freut sich über die neue Willkommensnachricht. " +
                     "Er gibt allerdings zu, dass diese einfache Aufgabe nur ein kleiner Test war um zu prüfen, " +
                     "ob du gewappnet für das Berufsleben als Schoggifabriksoftwareentwickler bist.</p>" +
@@ -48,6 +48,29 @@ namespace Schoggifabrik.Data
                     Match("Paul", "Willkommen Paul!"),
                     Match("Herr Gummi", "Willkommen Herr Gummi!"),
                     Match("Mr.  Sonderzeichen !&-*#@", "Willkommen Mr.  Sonderzeichen !&-*#@!"),
+                }),
+
+            new Problem(
+                name: "Statistiken",
+                flavor: "Die Willkommensnachricht zeigt jetzt allen Gästen persönliche Begrüssungen und deine Arbeit dafür ist getan. " +
+                    "Als nächstes braucht die Schoggiverpackungs-Abteilung deine Hilfe. Dort wurde kürzlich eine neue Maschine angeschafft, " +
+                    "welche für jede verpackte Schoggi die Verpackungsqualität misst und mit einem Wert von 0 - 100 angibt.</p>" +
+                    "<p>Du sollst aus diesen Werten nun <span class=\"highlight\">Statistiken</span> berechnen. Konkret sollen " +
+                    "<span class=\"highlight\">Minumum, Maximum, Durchschnitt, Median, Anzahl und Summe</span> " +
+                    "berechnet und in dieser Reihenfolge ausgegeben werden. Dein Arbeitskollege hat bereits die Eingabe programmiert.",
+                input: "Die 1. Inputzeile enthält eine Reihe von Zahlen q<sub>1</sub>, q<sub>2</sub>, ..., q<sub>n</sub> die jeweilige Verpackungsqualität. " +
+                    "Wobei 0 &le; q<sub>i</sub> &le; 100.</p>" +
+                    "<p>Beispiele: <code>55</code> oder <code>1 2 3 6</code>",
+                output: "Eine Reihe von Zahlen getrennt durch Leerzeichen. Nämlich: <span class=\"highlight\">" +
+                    "Minumum, Maximum, Durchschnitt, Median, Anzahl und Summe</span> der eingegebenen Qualitäten.</p>" +
+                    "<p>Beispiel Output: <code>55 55 55 55 1 55</code> oder <code>1 6 3 2.5 4 12</code>",
+                stubCode: "main = do\n"+
+                    "  values <- getLine >>= (return.fmap readInt.words)\n" +
+                    "  mapM_ print values\n\n" +
+                    "readInt :: String -> Int\n" +
+                    "readInt = read",
+                testCases: new Problem.TestCase[] {
+                    // TODO: Add testcases
                 }),
         };
     }
